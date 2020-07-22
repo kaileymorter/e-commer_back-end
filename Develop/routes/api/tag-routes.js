6991bfe9bdb2417ca469513c0227995800813956
Spartacus <k_morter@outlook.com>
2020-07-22 14:17:30 -0600

    added get route for tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,8 +4,22 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    //include its associated Product data
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+        through: ProductTag,
+        as: 'tagged_products'
+      }
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
